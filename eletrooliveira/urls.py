e33cd68376833d872318cf76a4dbde950f1062b8
Aruan Macena <professoraruan@gmail.com>
2026-03-31 00:01:27 -0300

    Criacao da pagina raiz do projeto

--- eletrooliveira/urls.py
+++ eletrooliveira/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/',views.index_eletro),
+    path('',RedirectView.as_view(url='index/')),
 ]
